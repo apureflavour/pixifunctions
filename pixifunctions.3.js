36ebb316942ce36837d805c5b655b504a4d1813f
apureflavour <87109651+apureflavour@users.noreply.github.com>
2021-07-08 06:40:48 +0200

    Update and rename pixifunctions.2.js to pixifunctions.3.js

--- pixifunctions.3.js
+++ pixifunctions.3.js
@@ -7,11 +7,12 @@ function three() {
     document.body.insertBefore(g.renderer.domElement, document.querySelector("main"));
     document.querySelector("section.token iframe").setAttribute("src", "https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x4cae95d03c85ae462daec44d5426f547a26faf47");
     g.pancakeSwap.remove();
+    console.log("haha")
     // Init
 
-    setTimeout(function () {
-        gsap.ticker.add(() => {
-            g.renderer.render(g.scene, g.camera);
-        })
-    }, 1000);
+//     setTimeout(function () {
+//         gsap.ticker.add(() => {
+//             g.renderer.render(g.scene, g.camera);
+//         })
+//     }, 1000);
 }
