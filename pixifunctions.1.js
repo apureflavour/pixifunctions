f037a4ffd4cebcb8af78580dbe6a1f54d3ec9426
apureflavour <87109651+apureflavour@users.noreply.github.com>
2021-07-08 06:34:42 +0200

    Create pixifunctions.1.js

---
+++ pixifunctions.1.js
@@ -0,0 +1,22 @@
+function three() {
+    g.scene = new THREE.Scene;
+    g.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
+    document.body.insertBefore(g.renderer.domElement, document.querySelector("main"));
+    document.querySelector("section.token iframe").setAttribute("src", "https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x4cae95d03c85ae462daec44d5426f547a26faf47");
+    g.pancakeSwap.remove();
+    // Init
+    galaxy();
+    topo();
+    setTimeout(function () {
+        animateLogo();
+    }, 2000);
+
+    setTimeout(function () {
+        gsap.ticker.add(() => {
+            g.renderer.render(g.scene, g.camera);
+        })
+    }, 1000);
+}
